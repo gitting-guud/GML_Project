43eb30687d5aab4b154d220952158ba6c824f766
gitting-guud <43947440+gitting-guud@users.noreply.github.com>
2019-12-24 02:13:35 +0100

    Completed_Env

--- Environment/Env.py
+++ Environment/Env.py
@@ -71,7 +71,10 @@ class Graph:
 
        # save current path if we found end
        if currVertex == destVertex:
-           fullPath.append({"path": list(path), "cost": vertex.currCost})
+           fullPath.append({
+                            "path": list(path),
+#                             "cost": vertex.currCost
+                           })
 
        for i in vertex.get_connections():
            if i not in visited:
@@ -101,17 +104,89 @@ class Agent_to_graph_assignment :
         for name in self.list_agents_names :
             dico = dict()
             dico["name"] = name
+            dico["infos"] = {}
             Path_not_exists = True 
             while Path_not_exists :
                 start_desti = np.random.choice(list(self.g.vertices.keys()), replace = False, size = 2)
                 start, destination = start_desti[0],start_desti[1]
                 if len(self.g.getAllPaths(start, destination)) > 0 :
                     Path_not_exists = False
-                    dico["start"], dico["destination"] = start, destination 
-                    dico["arms"] = self.g.getAllPaths(start, destination)
+                    dico["infos"]["start"], dico["infos"]["destination"] = start, destination 
+                    dico["infos"]["arms"] = self.g.getAllPaths(start, destination)
             self.agents_dicts.append(dico)
         
         return self.agents_dicts
     
     
+class cost_calculator :
+    
+    def __init__(self, list_arms_pulled, adj_matrix):
+        self.adj_matrix = adj_matrix.copy()
+        self.list_arms = list_arms_pulled.copy()
+
+        # set the initial costs of edges at the round t
+        self.edges_count_reset = {}
+        for i in range(self.adj_matrix.shape[0]):
+            for j in range(self.adj_matrix.shape[1]):
+                if self.adj_matrix[i,j]>0:
+#                     self.edges_count_reset[(i,j)] = 0
+                    self.edges_count_reset[(i,j)] = []
+        
+        
+    def reset(self):
+        for key in self.edges_count_reset.keys():
+            self.edges_count_reset[key].append(0)
+#         self.edges_count = self.edges_count_reset.copy()
+        
+    def padding(self, list_, nb_elements_to_pad):
+        return list_ + [-99]*nb_elements_to_pad
+    
+    def get_max_len(self):
+        max_len = -9999     
+        for L in self.list_arms :
+            l = len(L) 
+            if l > max_len :
+                max_len = l
+        return max_len
+    
+    def cost_calculation(self):
+
+        max_len = self.get_max_len()
+        self.list_arms_padded = []
+        self.costs_arms = np.zeros(len(self.list_arms))
+        
+        for list_ in self.list_arms :
+            self.list_arms_padded.append(self.padding(list_, max_len - len(list_)))
+        
+        self.list_arms_padded = np.array(self.list_arms_padded)
+        
+        for j in range(self.list_arms_padded.shape[1]-1):
+            self.reset()
+            edges_time_j_jplusone = self.list_arms_padded[:,[j,j+1]]
+            for i, row in enumerate(edges_time_j_jplusone) :
+                row_inversed = row[::-1]
+                tuple_row, tuple_row_inv = tuple(row), tuple(row_inversed)
+                if tuple_row in self.edges_count_reset.keys():
+                    # we have an undirected graph
+                    self.edges_count_reset[tuple_row][-1] += 1
+                    self.edges_count_reset[tuple_row_inv][-1] += 1
+            
+            for i, row in enumerate(edges_time_j_jplusone):
+                if tuple(row) in self.edges_count_reset.keys():
+                    self.costs_arms[i] += self.edges_count_reset[tuple(row)][j]
+      
+        return self.costs_arms
+                
+    def return_costs(self):
+        """Function that returns :
+        - the total cost of the arms passed as arguments during that iteration.
+        - the costs of each edge at each hop (hop = step towards the destination)
+        """
+        costs = self.cost_calculation()
+        results = {}
+        for i, arm in enumerate(self.list_arms):
+            results[i] = {"path" : arm, 'cost':costs[i]}
+            
+        return results, self.edges_count_reset
+        
 
