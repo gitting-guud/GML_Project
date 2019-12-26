c4fc14dde0fceb56c7a19d7a256ab0604445bacc
salmane-s9 <salmane.naoumi@gmail.com>
2019-12-26 21:12:20 +0100

    create FTRL agent

--- Agents/FTRL_agent.py
+++ Agents/FTRL_agent.py
@@ -9,6 +9,7 @@ class FTRL_agent_part1 :
         self.history_visited_edges = np.array(list(history_visited_edges.values())) # The agent's own count of the edges he visited
         self.arms_sto = arms_sto # The arms/paths to reach the destination 
         self.L_hat = np.zeros(len(self.arms_sto))
+        self.x = 0.5 * np.ones(len(self.arms_sto))
         self.dict_hat_costs_edges = dict_hat_costs_edges # the supposition on the costs of the edges the agent makes by himself, as a dict
         
         if not ((gamma >=0 ) and (gamma <= 1)):
@@ -16,22 +17,25 @@ class FTRL_agent_part1 :
         else :
             self.gamma = gamma # parameter of the UCB update
 
-    def upper_confidence_bound(self):
-        return NotImplementedError
-  
-    def update_L_hat(self, feedback):
-        
-        l_hat = None
-        self.L_hat += l_hat
-
-    def step(self):
-        return NotImplementedError
-    
     def get_learning_rate(self, t):
         return 1.0 / np.sqrt(t)
 
     def solve_optimization(self):
+
+        self.x = None
         return NotImplementedError
 
     def sample_action(self, x):
         return NotImplementedError
+
+    def step(self):
+        self.learning_rate = self.get_learning_rate(self.t)
+        self.solve_optimization()
+        action = self.sample_action(self.x)
+        self.t +=1
+        return action
+
+    def update_L_hat(self, action, feedback):
+        
+        l_hat = np.divide((np.array(feedback) + 1.0), self.x[action]) - 1
+        self.L_hat += l_hat
